0d08278f5eb299a362f7f8026a4ba519ba73c0ff
SSarah <hichammeftah4@gmail.com>
2022-09-05 22:57:51 +0100

    Finished with no bugsAdd project files.

---
+++ FizzBuzz.py
@@ -0,0 +1,31 @@
+
+# FizzBuzz from 42's exams made in python
+# difficulty level: Pretty Low
+# requirements: conditions, input, loops
+# Dev: SSarah - Sarah Hicham Meftah
+
+start = input("Put the starting number in the prompt:\n")
+
+if int(start) < 0 :
+    start = input("Put the starting number in the prompt:\n")
+
+end = input("Put the limit here:\n")
+
+if int(end) <= 0 :
+    end = input("Put the limit here:\n")
+
+startI = int(start)
+endI = int(end)
+
+while startI <= endI:
+    if startI == 0:
+        print(startI)
+    elif startI % 5 == 0 and startI % 3 == 0 :
+        print("fizzbuzz")
+    elif startI % 5 == 0 :
+        print("buzz")
+    elif startI % 3 == 0 :
+        print("fizz")
+    else :
+        print(startI)
+    startI += 1
